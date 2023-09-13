f154c353cfaefc949f132daaba6dcdad4a8a6524
GordanMilovac <gordan_milovac@brown.edu>
2023-09-13 10:19:22 -0400

    First changes

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Gordan Milovac',
+    site_url: 'https://gmilovac.com',
+    feed_url: 'https://gmilovac/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
