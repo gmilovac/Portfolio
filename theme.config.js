dcdb750780a1f9f7564d4c83bc3401e0ab74fa47
GordanMilovac <gordan_milovac@brown.edu>
2023-09-13 20:30:50 -0400

    burp

--- theme.config.js
+++ theme.config.js
@@ -4,15 +4,10 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Gordan Milovac.
-
         <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/linkedin.png" width="15" height="15"/></a>
-
         <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/github.png" width="15" height="15"/></a>
-
         <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/instagram.webp" width="15" height="15"/></a>
-
         <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/mail.png" width="17" height="17"/></a>
-
       <style jsx>{`
         @media screen and (max-width: 480px) {
           article {
