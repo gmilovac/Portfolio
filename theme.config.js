4ad329361e4265b452bf1c8b3d4150967dec5991
GordanMilovac <gordan_milovac@brown.edu>
2023-09-13 20:19:58 -0400

    bwfifqeob

--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Gordan Milovac.
-        <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="public/images/linkedin.png"/></a>
+      <time>{YEAR}</time> © Gordan Milovac. <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/linkedin.png"/></a>
         <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
