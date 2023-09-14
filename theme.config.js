90412661d2a8e131b43dfdf39c7396be32effbec
GordanMilovac <gordan_milovac@brown.edu>
2023-09-13 20:34:50 -0400

    wbef

--- theme.config.js
+++ theme.config.js
@@ -3,11 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Gordan Milovac.
-        <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/linkedin.png" width="15" height="15"/></a>
-        <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/github.png" width="15" height="15"/></a>
-        <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/instagram.webp" width="15" height="15"/></a>
-        <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/mail.png" width="17" height="17"/></a>
+      <time>{YEAR}</time> © Gordan Milovac.<a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/linkedin.png" width="15" height="15"/></a><a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/github.png" width="15" height="15"/></a><a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/instagram.webp" width="15" height="15"/></a><a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/mail.png" width="17" height="17"/></a>
         <style jsx>{`
         a {
           float: right;
