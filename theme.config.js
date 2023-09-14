e8439575899f3136567b94049dc50f15faeb811a
GordanMilovac <gordan_milovac@brown.edu>
2023-09-13 20:51:40 -0400

    hwld

--- theme.config.js
+++ theme.config.js
@@ -3,10 +3,12 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time>
-        © Gordan Milovac.
-        <a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/linkedin.png" width="15" height="15"/></a><a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/github.png" width="15" height="15"/></a><a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/instagram.webp" width="15" height="15"/></a><a target="_blank" href="https://www.linkedin.com/in/gmilovac/"><img src="/images/mail.png" width="17" height="17"/></a>
-        <style jsx>{`
+      <time>{YEAR}</time> © Gordan Milovac.
+      <a href="https://drive.google.com/file/d/14AqNL2MMT_UcSgGh71WNMrJTm2qjh2YL/view?usp=sharing">Resume</a>
+      <style jsx>{`
+        a {
+          float: right;
+        }
         @media screen and (max-width: 480px) {
           article {
             padding-top: 2rem;
